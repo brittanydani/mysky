a1289df4fa090d2f2920aa86d2b5a07894abc541
brittanydani <britdanielle8192@gmail.com>
2026-04-22 00:57:25 -0400

    chore: update app and eas configuration

--- app.config.js
+++ app.config.js
@@ -1,27 +1,42 @@
 const { expo: baseConfig } = require('./app.json');
 
-const APP_VARIANT = process.env.APP_VARIANT;
+const APP_VARIANT = process.env.APP_VARIANT ?? 'production';
 const isDevVariant = APP_VARIANT === 'development';
+const isPreviewVariant = APP_VARIANT === 'preview';
 const isLightweightDevMode = process.env.EXPO_PUBLIC_LIGHTWEIGHT_DEV === '1';
+
+let name = baseConfig.name;
+let scheme = baseConfig.scheme;
+let iosBundleIdentifier = baseConfig.ios?.bundleIdentifier;
+let androidPackage = baseConfig.android?.package;
+
+if (isDevVariant) {
+  name = 'MySky Dev';
+  scheme = 'mysky-dev';
+  iosBundleIdentifier = 'com.brittany.mysky.dev';
+  androidPackage = 'com.brittany.mysky.dev';
+} else if (isPreviewVariant) {
+  name = 'MySky Preview';
+  scheme = 'mysky-preview';
+  iosBundleIdentifier = 'com.brittany.mysky.preview';
+  androidPackage = 'com.brittany.mysky.preview';
+}
 
 module.exports = () => ({
   ...baseConfig,
-  name: isDevVariant ? 'MySky Dev' : baseConfig.name,
-  icon: baseConfig.icon,
-  scheme: isDevVariant ? 'mysky-dev' : baseConfig.scheme,
+  name,
+  scheme,
   ios: {
     ...baseConfig.ios,
-    bundleIdentifier: isDevVariant
-      ? 'com.brittany.mysky.dev'
-      : baseConfig.ios.bundleIdentifier,
+    bundleIdentifier: iosBundleIdentifier,
   },
   android: {
     ...baseConfig.android,
-    package: isDevVariant ? 'com.brittany.mysky.dev' : baseConfig.android?.package,
+    package: androidPackage,
   },
   extra: {
     ...baseConfig.extra,
-    appVariant: isDevVariant ? 'development' : 'production',
+    appVariant: APP_VARIANT,
     lightweightDevMode: isLightweightDevMode,
   },
 });
